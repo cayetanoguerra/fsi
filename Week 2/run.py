c6612bff2c9f6a85bf3bf2693be887f858b315ff
Cayetano <cayetanoguerra@gmail.com>
2016-03-02 00:26:22 +0000

    compare

--- Week 2/run.py
+++ Week 2/run.py
@@ -13,8 +13,6 @@ print search.breadth_first_graph_search(ab).path()
 #print search.iterative_deepening_search(ab).path()
 #print search.depth_limited_search(ab).path()
 
-search.compare_graph_searchers()
-
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
 # [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
